dcd18f91c3505b0bc665fa32fec954252fe9dec0
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 21:09:56 +0300

    feat: create User.java

---
+++ src/main/java/com/example/projectmanagementtools/Entity/User.java
@@ -0,0 +1,18 @@
+package com.example.projectmanagementtools.Entity;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class User{
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private Timestamp createdAt;
+}
