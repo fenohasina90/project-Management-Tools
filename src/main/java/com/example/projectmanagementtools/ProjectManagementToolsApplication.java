a879c659216c228c2619f1d8e9ec519263e1b1fe
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 14:50:34 +0300

    initialize commit

---
+++ src/main/java/com/example/projectmanagementtools/ProjectManagementToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementToolsApplication.class, args);
+    }
+
+}
