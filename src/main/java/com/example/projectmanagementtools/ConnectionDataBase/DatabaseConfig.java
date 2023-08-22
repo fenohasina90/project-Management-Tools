dec959eefb99e26a066b398c4232efd94b3eea33
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 17:20:07 +0300

    feat: create and config DatabaseConfig.java

---
+++ src/main/java/com/example/projectmanagementtools/ConnectionDataBase/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.projectmanagementtools.ConnectionDataBase;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+
+@Component
+public class DatabaseConfig {
+    @Value("${DB_URL}")
+    private String Url;
+
+    @Value("${DB_USERNAME}")
+    private String Username;
+
+    @Value("${DB_PASSWORD}")
+    private String Password;
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(this.Url, this.Username, this.Password);
+    }
+}
