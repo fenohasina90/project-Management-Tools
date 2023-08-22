c6b706d1b55906ea690ba803d3b0bbafd886d874
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 21:54:32 +0300

    feat: create BasisDAO.java

---
+++ src/main/java/com/example/projectmanagementtools/Repository/BasisDAO.java
@@ -0,0 +1,21 @@
+package com.example.projectmanagementtools.Repository;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+
+public abstract class BasisDAO<T>{
+    private Connection connection;
+    public BasisDAO(Connection connection) {this.connection = connection;}
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public abstract void insert(T toInsert) throws SQLException;
+    public abstract List<T> findAll() throws  SQLException;
+    public abstract Optional<T> findById(int id) throws SQLException;
+    public abstract void update(T toUpdate) throws SQLException;
+    public abstract void delete(int id) throws SQLException;
+}
