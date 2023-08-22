79d4907ea5c6329b45b7c519542df5ee800c84f9
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 23:51:43 +0300

    feat: add findAll method

--- src/main/java/com/example/projectmanagementtools/Repository/TaskDAO.java
+++ src/main/java/com/example/projectmanagementtools/Repository/TaskDAO.java
@@ -2,9 +2,8 @@ package com.example.projectmanagementtools.Repository;
 
 import com.example.projectmanagementtools.Entity.Task;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +15,7 @@ public class TaskDAO extends BasisDAO<Task>{
     public void insert(Task toInsert) throws SQLException {
         String sql = "INSERT INTO \"task\""+
                 " (task_name,description,deadline,task_status,created_at,updated_at,created_by,for_project)"+
-                        " VALUES (?,?,?,?,?,?);";
+                        " VALUES (?,?,?,?,?,?,?,?);";
 
         try(PreparedStatement preparedStatement = getConnection().prepareStatement(sql)){
             preparedStatement.setString(1, toInsert.getTaskName());
@@ -37,7 +36,28 @@ public class TaskDAO extends BasisDAO<Task>{
 
     @Override
     public List<Task> findAll() throws SQLException {
-        return null;
+        List<Task> list = new ArrayList<>();
+        String sql = "SELECT * FROM \"task\";";
+
+        try (Statement statement = getConnection().createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)){
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String taskName = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                Date deadline = resultSet.getDate(4);
+                String taskStatus = resultSet.getString(5);
+                Timestamp createdAt = resultSet.getTimestamp(6);
+                Timestamp updatedAt = resultSet.getTimestamp(7);
+                int userId = resultSet.getInt(8);
+                int projectId = resultSet.getInt(9);
+
+                Task task = new Task(id,taskName,description,deadline,taskStatus,createdAt,updatedAt,userId,projectId);
+                list.add(task);
+            }
+        }
+        return list;
     }
 
     @Override
