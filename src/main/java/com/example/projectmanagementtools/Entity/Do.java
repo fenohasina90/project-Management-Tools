c50782968e22a80f7361238b45fcbcd81fa979e1
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 21:08:10 +0300

    feat: create do.java

---
+++ src/main/java/com/example/projectmanagementtools/Entity/Do.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementtools.Entity;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Do {
+    private int userId;
+    private int projectId;
+}
