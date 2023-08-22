680c91065a2b55f3b28dca859ff68d81f286d655
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-22 23:58:37 +0300

    feat: add findById method

--- src/main/java/com/example/projectmanagementtools/Repository/TaskDAO.java
+++ src/main/java/com/example/projectmanagementtools/Repository/TaskDAO.java
@@ -1,12 +1,14 @@
 package com.example.projectmanagementtools.Repository;
 
 import com.example.projectmanagementtools.Entity.Task;
+import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class TaskDAO extends BasisDAO<Task>{
 
     public TaskDAO(Connection connection) {super(connection);}
@@ -62,6 +64,28 @@ public class TaskDAO extends BasisDAO<Task>{
 
     @Override
     public Optional<Task> findById(int id) throws SQLException {
+        String sql = "SELECT * FROM \"task\" WHERE id_task = ?;";
+
+        try(PreparedStatement preparedStatement = getConnection().prepareStatement(sql)){
+            preparedStatement.setInt(1,id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()){
+                    int idTask = resultSet.getInt(1);
+                    String taskName = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    Date deadline = resultSet.getDate(4);
+                    String taskStatus = resultSet.getString(5);
+                    Timestamp createdAt = resultSet.getTimestamp(6);
+                    Timestamp updatedAt = resultSet.getTimestamp(7);
+                    int userId = resultSet.getInt(8);
+                    int projectId = resultSet.getInt(9);
+
+                    Task task = new Task(idTask,taskName,description,deadline,taskStatus,createdAt,updatedAt,userId,projectId);
+                    return Optional.of(task);
+                }
+            }
+
+        }
         return Optional.empty();
     }
 
