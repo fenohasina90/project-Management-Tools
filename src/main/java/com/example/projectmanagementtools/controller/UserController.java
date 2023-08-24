26b9672d947afc97e4bd78542d765aa1de1b25a5
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-24 11:02:05 +0300

    feat: UserController added with CRUD methods

---
+++ src/main/java/com/example/projectmanagementtools/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.projectmanagementtools.controller;
+
+import com.example.projectmanagementtools.entity.User;
+import com.example.projectmanagementtools.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {this.userService = userService;}
+
+    @GetMapping("/allUser")
+    public List<User> getAllUser(){
+        return this.userService.findAllUser();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUserById(@PathVariable int id){
+        return this.userService.findUserById(id);
+    }
+
+    @PostMapping("/addUser")
+    public User insertUser(@RequestBody User toInsert){
+        return this.userService.insert(toInsert);
+    }
+
+    @PutMapping("/updateUser/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User updatedUser) {
+        if (updatedUser.getId() != id) {
+            throw new IllegalArgumentException("The ID provided in the request body does not match the ID in the path.");
+        }
+        return this.userService.updateUser(updatedUser);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("Deleted with success!");
+    }
+
+}
